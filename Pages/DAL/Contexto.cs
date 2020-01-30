9c49fe630e8bba0ed4d1059ab22767e600788869
Ariceidy Maria Ure�a Rosario <ariceidy_urena@ucne.edu.do>
2020-01-29 20:25:13 -0400

    Primera parte del proyecto final lista con el registro de usuarios realizado

---
+++ Pages/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFnal.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFnal.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(@"Server=.\SqlExpress;Database=ProyectoFerreteriaDb;truested_Connection =true"));
+        }
+    }
+}
---
+++ Pages/Entidades/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFnal.Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Nombres { get; set; }
+        public string Email { get; set; }
+        public string NivelUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+    }
+}
